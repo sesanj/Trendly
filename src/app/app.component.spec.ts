5b307eef3186dcfb84fbd972fe1250ff36449d9a
aaravrathod <W0838110@myscc.ca>
2025-01-18 22:57:53 -0400

    Merging conflicts fixed

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -2,39 +2,28 @@ import { TestBed } from '@angular/core/testing';
 import { AppComponent } from './app.component';
 
 describe('AppComponent', () => {
-  beforeEach(async () => {
-    await TestBed.configureTestingModule({
-      imports: [AppComponent],
-    }).compileComponents();
-  });
+    beforeEach(async () => {
+        await TestBed.configureTestingModule({
+            imports: [AppComponent],
+        }).compileComponents();
+    });
 
-  it('should create the app', () => {
-    const fixture = TestBed.createComponent(AppComponent);
-    const app = fixture.componentInstance;
-    expect(app).toBeTruthy();
-  });
+    it('should create the app', () => {
+        const fixture = TestBed.createComponent(AppComponent);
+        const app = fixture.componentInstance;
+        expect(app).toBeTruthy();
+    });
 
-<<<<<<< HEAD
-  it(`should have the 'WebProject' title`, () => {
-    const fixture = TestBed.createComponent(AppComponent);
-    const app = fixture.componentInstance;
-    expect(app.title).toEqual('WebProject');
-=======
-  it(`should have the 'Trendly' title`, () => {
-    const fixture = TestBed.createComponent(AppComponent);
-    const app = fixture.componentInstance;
-    expect(app.title).toEqual('Trendly');
->>>>>>> 3317c231c89db792a5bb85cd888b3081c5f8bb6b
-  });
+    it(`should have the 'Trendly' title`, () => {
+        const fixture = TestBed.createComponent(AppComponent);
+        const app = fixture.componentInstance;
+        expect(app.title).toEqual('Trendly');
+    });
 
-  it('should render title', () => {
-    const fixture = TestBed.createComponent(AppComponent);
-    fixture.detectChanges();
-    const compiled = fixture.nativeElement as HTMLElement;
-<<<<<<< HEAD
-    expect(compiled.querySelector('h1')?.textContent).toContain('Hello, WebProject');
-=======
-    expect(compiled.querySelector('h1')?.textContent).toContain('Hello, Trendly');
->>>>>>> 3317c231c89db792a5bb85cd888b3081c5f8bb6b
-  });
+    it('should render title', () => {
+        const fixture = TestBed.createComponent(AppComponent);
+        fixture.detectChanges();
+        const compiled = fixture.nativeElement as HTMLElement;
+        expect(compiled.querySelector('h1')?.textContent).toContain('Hello, Trendly');
+    });
 });
