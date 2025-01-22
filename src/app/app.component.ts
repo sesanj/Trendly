1f47253ef5118094843222a5a3475389d25224cf
aaravrathod <W0838110@myscc.ca>
2025-01-22 18:29:27 -0400

    Added back original css and html of App componenet

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,12 @@
 
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import {FeaturesComponent} from "./features/features.component";
+import {HeaderComponent} from "./header/header.component";
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, FeaturesComponent, HeaderComponent],
   templateUrl: './app.component.html',
   standalone: true,
   styleUrl: './app.component.css'
