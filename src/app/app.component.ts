783da760179000ece37b89fa2b944e231e801c11
aaravrathod <W0838110@myscc.ca>
2025-02-10 14:12:13 -0400

    Register Form Completed

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,11 +1,12 @@
 import { Component, inject } from '@angular/core';
 import { ProductServiceService } from './product service/product-service.service';
 import { HomeComponent } from './home/home.component';
+import {RegisterComponent} from "./register/register.component";
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [HomeComponent],
+  imports: [HomeComponent, RegisterComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css',
 })
