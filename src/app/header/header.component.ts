3703261420a01f5b122630c655df17c339459aa6
Sesan Joel <w0836046@myscc.ca>
2025-02-28 12:24:10 -0500

    updated

--- src/app/header/header.component.ts
+++ src/app/header/header.component.ts
@@ -1,19 +1,19 @@
 import { Component } from '@angular/core';
-import {Router, RouterLink, RouterLinkActive, RouterOutlet} from "@angular/router";
+import {
+  Router,
+  RouterLink,
+  RouterLinkActive,
+  RouterOutlet,
+} from '@angular/router';
 
 @Component({
   selector: 'app-header',
   standalone: true,
-  imports: [
-    RouterLink,
-    RouterLinkActive,
-    RouterOutlet
-  ],
+  imports: [RouterLink, RouterLinkActive, RouterOutlet],
   templateUrl: './header.component.html',
   styleUrl: './header.component.css',
 })
 export class HeaderComponent {
-
   total: number = 127.06;
   constructor(private router: Router) {}
 }
