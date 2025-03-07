fefb1c8bfcabcb2b8b072814e61e72a1d30c05f8
Lujia Yang <w0841073@myscc.ca>
2025-03-06 22:37:01 -0500

    update

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,12 +2,13 @@ import { Component } from '@angular/core';
 import { HeaderComponent } from './header/header.component';
 import { FooterComponent } from './footer/footer.component';
 import { Router, RouterOutlet } from '@angular/router';
+import {CheckoutComponent} from "./shoppingcart/shoppingcart.component";
 
 @Component({
   selector: 'app-root',
   standalone: true,
 
-  imports: [HeaderComponent, FooterComponent, RouterOutlet],
+  imports: [HeaderComponent, FooterComponent, RouterOutlet, CheckoutComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css',
 })
