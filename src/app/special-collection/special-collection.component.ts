6ce3a1ac1a122137611ff78cd9ba2d9d28927d8c
Sesan Joel <w0836046@myscc.ca>
2025-04-08 10:04:29 -0400

    Review section and special items section optimized

--- src/app/special-collection/special-collection.component.ts
+++ src/app/special-collection/special-collection.component.ts
@@ -14,11 +14,34 @@ export class SpecialCollectionComponent {
   productService = inject(ProductServiceService);
   allProducts: Product[] = this.productService.getAllProducts();
 
-  product: Product[] = [this.allProducts[7], this.allProducts[16]];
+  product: Product[] = [this.allProducts[5], this.allProducts[13]];
 
-  navStyle: string = '';
+  navStyle: string = 'NEW';
 
-  getNew() {}
+  ngOnInit() {
+    this.getNew();
+  }
 
-  getHot() {}
+  getNew() {
+    let newProducts = this.allProducts.filter((item) => item.tag == 'NEW');
+
+    this.product[0] = newProducts[this.randomNumber(newProducts.length - 1, 0)];
+    this.product[1] = newProducts[this.randomNumber(newProducts.length - 1, 0)];
+
+    this.navStyle = 'NEW';
+  }
+
+  getHot() {
+    let hotProduct = this.allProducts.filter((item) => item.tag == 'HOT');
+
+    this.product[0] = hotProduct[this.randomNumber(hotProduct.length - 1, 0)];
+    this.product[1] = hotProduct[this.randomNumber(hotProduct.length - 1, 0)];
+    this.navStyle = 'HOT';
+  }
+
+  randomNumber(max: number, min: number) {
+    let number = Math.floor(Math.random() * (max - min + 1));
+
+    return number;
+  }
 }
