c05f30ec1e0a62f46e853410a8531199c532e2fa
Lujia Yang <w0841073@myscc.ca>
2025-04-09 22:21:44 -0400

    mobile cart and category section

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,13 @@
 import { Component } from '@angular/core';
 import { Router, RouterOutlet } from '@angular/router';
+import {CartComponent} from "./cart/cart.component";
+import {CardSectionComponent} from "./card-section/card-section.component";
+import {CategorySectionComponent} from "./category-section/category-section.component";
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, CartComponent, CardSectionComponent, CategorySectionComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css',
 })
